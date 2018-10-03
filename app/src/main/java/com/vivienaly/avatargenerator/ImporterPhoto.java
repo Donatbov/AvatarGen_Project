cdbd600c81c857ba661d938135b94c669a073865
vivien <viviendonatb@gmail.com>
2018-10-03 10:40:50 +0200

    refactoring  code

--- app/src/main/java/com/vivienaly/avatargenerator/ImporterPhoto.java
+++ app/src/main/java/com/vivienaly/avatargenerator/ImporterPhoto.java
@@ -25,16 +25,8 @@ public class ImporterPhoto extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_import_photo);
-        importer = (Button) findViewById(R.id.importer_photo);
-        import_view =(ImageView) findViewById(R.id.import_view);
-       /* importer.setOnClickListener(new View.OnClickListener() {
-            public void onClick(View v) {
-                if (v.getId() == R.id.importer_photo) { // C'est notre bouton alors affichage d'un message
-                    Toast.makeText(getApplicationContext(), "bouton ok", Toast.LENGTH_SHORT).show();
-
-                }
-            }
-        });*/
+        importer = findViewById(R.id.importer_photo);
+        import_view = findViewById(R.id.import_view);
     }
 
     public void btnClick(View v) {
@@ -43,56 +35,8 @@ public class ImporterPhoto extends AppCompatActivity {
 
     }
 
+
     @Override
-   /* protected void onActivityResult(int requestCode, int resultCode, Intent data) {
-
-        super.onActivityResult(requestCode, resultCode, data);
-
-        switch (requestCode) {
-            case SELECTED_PIC:
-                if (resultCode == RESULT_OK) {
-                    Uri uri = data.getData();
-                    String[] projection = {MediaStore.Images.Media.DATA};
-
-                    Cursor cursor = getContentResolver().query(uri, projection, null, null, null);
-                    cursor.moveToFirst();
-
-                    int columnIndex = cursor.getColumnIndex(projection[0]);
-                    String filepath = cursor.getString(columnIndex);
-                    cursor.close();
-
-                    Bitmap bitmap = BitmapFactory.decodeFile(filepath);
-                    Drawable drawable = new BitmapDrawable(bitmap);
-                    import_view.setBackground(drawable);
-                }
-                break;
-            default:
-                break;
-        }
-    }
-    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
-        super.onActivityResult(requestCode, resultCode, data);
-
-        // Here we need to check if the activity that was triggers was the Image Gallery.
-        // If it is the requestCode will match the LOAD_IMAGE_RESULTS value.
-        // If the resultCode is RESULT_OK and there is some data we know that an image was picked.
-        if (requestCode == SELECTED_PIC && resultCode == RESULT_OK && data != null) {
-            // Let's read picked image data - its URI
-            Uri pickedImage = data.getData();
-            // Let's read picked image path using content resolver
-            String[] filePath = { MediaStore.Images.Media.DATA };
-            Cursor cursor = getContentResolver().query(pickedImage, filePath, null, null, null);
-            cursor.moveToFirst();
-            String imagePath = cursor.getString(cursor.getColumnIndex(filePath[0]));
-
-            // Now we need to set the GUI ImageView data with data read from the picked file.
-            import_view.setImageBitmap(BitmapFactory.decodeFile("/storage/emulated/0/DCIM/Camera/IMG_20180925_144106.jpg"));
-            Toast.makeText(getApplicationContext(),imagePath , Toast.LENGTH_SHORT).show();
-            // At the end remember to close the cursor or you will end with the RuntimeException!
-            cursor.close();
-        }
-    }*/
-
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
         try {
@@ -116,7 +60,7 @@ public class ImporterPhoto extends AppCompatActivity {
 
                 import_view.setImageBitmap(BitmapFactory
                         .decodeFile(ImageDecode));
-                Toast.makeText(this, "tres bien"+ImageDecode, Toast.LENGTH_LONG)
+                Toast.makeText(this, "tres bien" + ImageDecode, Toast.LENGTH_LONG)
                         .show();
 
             }
