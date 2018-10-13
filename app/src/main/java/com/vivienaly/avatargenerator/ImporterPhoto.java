6547973fc2c154ac29c26798c6b183ecfb91eae6
vivien <viviendonatb@gmail.com>
2018-10-13 13:54:41 +0200

    beginning of example implementation (/!\ errors with library import) warning: build.gradle(Module: app) changing and cannot be commit (in the gitignore file)

--- app/src/main/java/com/vivienaly/avatargenerator/ImporterPhoto.java
+++ app/src/main/java/com/vivienaly/avatargenerator/ImporterPhoto.java
@@ -4,8 +4,9 @@ import android.content.Intent;
 import android.database.Cursor;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
-import android.graphics.drawable.BitmapDrawable;
-import android.graphics.drawable.Drawable;
+import android.graphics.Canvas;
+import android.graphics.Paint;
+import android.media.FaceDetector;
 import android.net.Uri;
 import android.provider.MediaStore;
 import android.support.v7.app.AppCompatActivity;
@@ -14,24 +15,54 @@ import android.view.View;
 import android.widget.ImageView;
 import android.widget.Button;
 import android.widget.Toast;
+/*import com.google.android.gms.vision.face.Face;
+import com.google.android.gms.vision.face.FaceDetector;
+import com.google.android.gms.vision.Frame;*/
 
 public class ImporterPhoto extends AppCompatActivity {
     private static final int SELECTED_PIC = 1;
 
     Button importer;
+    Button btnProcess;
     ImageView import_view;
     String ImageDecode;
+    Canvas canvas;
+    Bitmap eyeBitmap;
+    Bitmap myBitmap;
+
+    Paint rectPaint = new Paint();
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_import_photo);
         importer = findViewById(R.id.importer_photo);
+        btnProcess = findViewById(R.id.btnProcess);
         import_view = findViewById(R.id.import_view);
+
+        btnProcess.setActivated(false);
     }
 
     public void btnClick(View v) {
         Intent intent = new Intent(Intent.ACTION_PICK, android.provider.MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
         startActivityForResult(intent, SELECTED_PIC);
+
+    }
+
+    public void BtnProcessClick(View v){
+        myBitmap = BitmapFactory.decodeResource(getApplicationContext().getResources(),R.drawable.faceform_0);
+        eyeBitmap = BitmapFactory.decodeResource(getResources(),R.drawable.faceform_0);
+        import_view.setImageBitmap(myBitmap);
+
+        Bitmap tempBitmap = Bitmap.createBitmap(myBitmap.getWidth(), myBitmap.getHeight(), Bitmap.Config.RGB_565);
+        canvas = new Canvas(tempBitmap);
+        canvas.drawBitmap(myBitmap,0,0,null);
+
+        FaceDetector faceDetector = new FaceDetector.Builder(getApplicationContext())
+                .setTrackingEnabled(false)
+                .setLandmarkType(FaceDetector.ALL_LANDMARKS)
+                .setMode(FaceDetector.FAST_MODE)
+                .build();
 
     }
 
@@ -62,6 +93,7 @@ public class ImporterPhoto extends AppCompatActivity {
                         .decodeFile(ImageDecode));
                 Toast.makeText(this, ImageDecode, Toast.LENGTH_LONG)
                         .show();
+                btnProcess.setActivated(true);
 
             }
         } catch (Exception e) {
